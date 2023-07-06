22c8a8ea7abde09c8230b1bf04b3b2d274e0ffea
Nebykov Artem <116262132+NBKRTM@users.noreply.github.com>
2023-07-07 02:04:06 +0600

    updated ingredientviewset

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -21,8 +21,19 @@ class IngredientSerializer(serializers.ModelSerializer):
 
 
 class RecipeIngredient(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField(
+        source='ingredient.id'
+    )
+    name = serializers.ReadOnlyField(
+        source='ingredient.name'
+    )
+    measurement_unit = serializers.ReadOnlyField(
+        source='ingredient.measurement_unit'
+    )
+
     class Meta:
         model = RecipeIngredient
+        fields = ['id', 'name', 'measurement_unit', 'amount']
 
 
 class RecipeSerializer(serializers.ModelSerializer):
--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import permissions
+from rest_framework import permissions, filters
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from recipes.models import (Tag,
                             Ingredient,
@@ -20,3 +20,5 @@ class IngredientViewSet(ReadOnlyModelViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
     permission_classes = [permissions.AllowAny]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
