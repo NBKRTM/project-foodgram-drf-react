c31d39d176200c902ceb4d8c42604ece455e2e64
Nebykov Artem <116262132+NBKRTM@users.noreply.github.com>
2023-07-17 18:42:54 +0300

    updated permissions, urls

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -12,5 +12,6 @@ router.register(r'users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('', include('djoser.urls.jwt')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
