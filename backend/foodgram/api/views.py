ee6168e03ec925e22b46af259a8b4a9e5097323c
Nebykov Artem <116262132+NBKRTM@users.noreply.github.com>
2023-08-06 22:34:05 +0300

    updated views #5

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.db.models import Sum
-from requests import Response
 from rest_framework import filters, permissions, status, viewsets
+from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.viewsets import ReadOnlyModelViewSet
